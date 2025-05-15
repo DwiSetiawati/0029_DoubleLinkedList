e7cd3b46662497f6fbe74c3b706057203fe7d180
DwiSetiawati <dwisetwa27@gmail.com>
2025-05-15 12:21:51 +0700

    Membuat node *currentNode = START pada fungsi traverse

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -135,5 +135,20 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+
+        // step 1: Mark first node as currentNode
+        Node *currentNode = START;
+
+        // step 2: Repeat until currentNode == NULL
+        cout << "\nRecords in ascending order od roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // step 3: Move to next node
+            currentNode = currentNode->next;
+            i++;
+        }
     }
 };
